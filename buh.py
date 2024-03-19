6a71d7ea881bfd434250eee5a3ea81a9997889e1
Andrey Shchepetov <shchepetov2012@yandex.ru>
2024-03-19 07:27:06 +0300

    Исправил ошибки в коде

--- buh.py
+++ buh.py
@@ -1,7 +1,7 @@
 import os
 
 first_file_name = input("Введите название первого файла:")
-last_file_name = input("Введите название второго файла:")
+last_file_name = input("Введите название последнего файла:")
 
 first_number = int(first_file_name.split(".")[0])
 last_number = int(last_file_name.split(".")[0])  # Извлекаем номера из файлов
@@ -12,10 +12,10 @@ if not os.path.exists(folder_path):
     print(f"Папка {folder_path} создана")
 
 for number in range(first_number, last_number + 1):
-    curent_file_name = f"files {number}.txt"  # Создаем файлы
-    with open(curent_file_name, 'w', encoding="utf-8") as file:  # Записываем файлы
-        file.write(f"Содержимое файла {curent_file_name}")
-    print(f"Файл {curent_file_name} создан")
+    current_file_name = f"files/{number}.txt"  # Создаем файлы
+    with open(current_file_name, 'w', encoding="utf-8") as file:  # Записываем файлы
+        file.write(f"Содержимое файла {current_file_name}")
+    print(f"Файл {current_file_name} создан")
 
 
 def copy_file(client_path, credit_path):  # Копируем файлы в файл for_buh.txt
